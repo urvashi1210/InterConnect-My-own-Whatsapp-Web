eaf05c27d717b26ab63810403d597f5de876f739
urvashi1210 <urvashishukla1210@gmail.com>
2023-07-03 22:15:34 +0530

    changing URIs

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const url='http://localhost:8000';
+// const url='http://localhost:8000';
+const url='https://interconnect-whatsapp-web-clone-api.onrender.com/';
 
 
 export const addUser=async (data)=>{
