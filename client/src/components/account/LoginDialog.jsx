06d25bc0428adad77f6ac92e9686a4b72542fb11
urvashi1210 <urvashishukla1210@gmail.com>
2023-07-03 22:02:39 +0530

    logout feature enhanced

--- client/src/components/account/LoginDialog.jsx
+++ client/src/components/account/LoginDialog.jsx
@@ -10,91 +10,101 @@ import {GoogleLogin} from '@react-oauth/google';
 import jwt_decode from 'jwt-decode';
 
 
-const Component=styled(Box)`
-display:flex;
-`
+const Component = styled(Box)`
+    display: flex; 
+`;
 
-const Container=styled(Box)`
-padding:56px 0 56px 56px
-`
-// Since img is not a material UI component, use it as a string
-const QRCode=styled('img')({
-    height:264,
-    width:264,
-    margin:'50px 0px 0px 300px',
-})
+const Container = styled(Box)`
+    padding: 56px 0 56px 56px;
+`;
 
-const Title=styled(Typography)`
-font-size:30px;
-color:#525252;
-font-weight:300;
-font-family:inherit;
-margin-left:70px;
-margin-bottom:25px;
-`
+const QRCOde = styled('img')({
+    margin: '50px 0 0 50px',
+    height: 264,
+    width: 264
+});
 
-const StyledList=styled(List)`
-&>li{
-    padding:0;
-    margin-left:100px;
-    margin-top:15px;
-    font-size:18px;
-    line-height:30px;
-    color:#111111;
+const Title = styled(Typography)`
+    font-size: 26px;
+    margin-bottom: 25px;
+    color: #525252;
+    font-family: Segoe UI,Helvetica Neue,Helvetica,Lucida Grande,Arial,Ubuntu,Cantarell,Fira Sans,sans-serif;
+    font-weight: 300;
+`;
+
+const StyledList = styled(List)`
+    &  > li {
+        padding: 0;
+        margin-top: 15px;
+        font-size: 18px;
+        line-height: 28px;
+        color: #4a4a4a;
+    }
+`;
+
+const dialogStyle = {
+    marginTop: '12%',
+    height: '95%',
+    width: '60%',
+    maxWidth: '100',
+    maxHeight: '100%',
+    borderRadius: 0,
+    boxShadow: 'none',
+    overflow: 'hidden'
 }
-`
 
-const dialogStyle={
-    height:'96%',
-    marginTop:'27%',
-    width:'85%',
-    maxWidth:'100%',
-    maxHeight:'100%',
-    boxShadow:'none',
-    overflow:'hidden'
-}
+const LoginDialog = () => {
 
-const LoginDialog=()=>{
+    const { setAccount,showloginButton, setShowloginButton, setShowlogoutButton } = useContext(AccountContext);
 
-    const {setAccount}=useContext(AccountContext);
-
-    const onLoginSuccess=async (res)=>{
-        const decoded=jwt_decode(res.credential);
+    const onLoginSuccess = async (res) => {
+        let decoded = jwt_decode(res.credential);
         setAccount(decoded);
+        setShowloginButton(false);
+        setShowlogoutButton(true);
         await addUser(decoded);
-    }
+    };
 
-    const onLoginError=(res)=>{
-        console.log('Login failed',res);
-    }
+    const onLoginFailure = (res) => {
+        console.log('Login Failed:', res);
+    };
 
-    return(
-       <Dialog
+    // const onSignoutSuccess = () => {
+    //     alert("You have been logged out successfully");
+    //     console.clear();
+    //     setShowloginButton(true);
+    //     setShowlogoutButton(false);
+    // };
+
+    return (
+        <Dialog
             open={true}
-            PaperProps={{sx:dialogStyle}}
-            hideBackdrop={true}
-            >
+            BackdropProps={{style: {backgroundColor: 'unset'}}}
+            maxWidth={'md'}
+            PaperProps={{ sx: dialogStyle }}
+        >
             <Component>
                 <Container>
-                    <Title>Use WhatsApp on your computer</Title>
+                    <Title>To use WhatsApp on your computer:</Title>
                     <StyledList>
-                        <ListItem>1. Open WhatsApp on your phone.</ListItem>
-                        <ListItem>2. Tap Menu ⁝ or Settings ◎ and select Linked Devices.</ListItem>
-                        <ListItem>3. Tap to link a device.</ListItem>
-                        <ListItem>4. Point your phone to this screen to capture the code.</ListItem>
+                        <ListItem>1. Open WhatsApp on your phone</ListItem>
+                        <ListItem>2. Tap Menu Settings and select WhatsApp Web</ListItem>
+                        <ListItem>3. Point your phone to this screen to capture the code</ListItem>
                     </StyledList>
                 </Container>
-                <Box fontStyle={{position:'relative'}}>
-                        <QRCode src={qrCodeImage} alt="qr code"/>
-                        <Box fontStyle={{position:'absolute',top:'40%',transform:'translateX(128%)'}}>
+                <Box style={{position:'relative'}}>
+                    <QRCOde src={qrCodeImage} alt="QR Code" />
+                    <Box style={{position: 'absolute', top: '50%', transform: 'translateX(25%) translateY(-25%)'}}>
+                        { showloginButton ?
                             <GoogleLogin
-                            onSuccess={onLoginSuccess}
-                            onError={onLoginError}
-                            />
-                        </Box>
+                                buttonText=""
+                                onSuccess={onLoginSuccess}
+                                onError={onLoginFailure}
+                            /> : null}
+                    </Box>
                 </Box>
             </Component>
-       </Dialog>
+        </Dialog>
     )
 }
 
