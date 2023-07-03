35e58ad7242e5eb6f724d35bc61a5d6883805bbb
urvashi1210 <urvashishukla1210@gmail.com>
2023-07-03 12:45:30 +0530

    build

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app=express();
 
 app.use(
     cors({
-      origin: ['http://localhost:3000','http://localhost:8000']
+      origin: ['http://localhost:3000','http://localhost:8000',"https://interconnect.onrender.com"]
     })
   );
 
