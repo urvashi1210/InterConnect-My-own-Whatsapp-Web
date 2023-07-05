521ff6d9f9f7888f84f2db75dc55cd271253aaf3
urvashi1210 <urvashishukla1210@gmail.com>
2023-07-05 22:58:40 +0530

    uri

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const url='http://localhost:8000';
-const url='https://interconnect-whatsapp-web-clone-api.onrender.com/';
+const url='https://interconnect-whatsapp-web-clone-api.onrender.com';
 
 
 export const addUser=async (data)=>{
