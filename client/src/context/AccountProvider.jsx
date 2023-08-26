a23aa93da97b2a08a83c608521850cf1dad0ab70
urvashi1210 <urvashishukla1210@gmail.com>
2023-08-26 18:46:14 +0530

    handling socket.io

--- client/src/context/AccountProvider.jsx
+++ client/src/context/AccountProvider.jsx
@@ -19,9 +19,9 @@ const socket=useRef();
 useEffect(() => {
     // Determine the WebSocket URL based on the current environment
     const socketURL =
-      process.env.NODE_ENV == `production`
-        ? 'https://interconnect-my-own-whatsapp-web-socket.onrender.com'
-        : 'ws://localhost:9000';
+    process.env.NODE_ENV === 'production'
+    ? 'https://interconnect-my-own-whatsapp-web-socket.onrender.com'
+    : 'http://localhost:9000';
 
     socket.current = io(socketURL);
 
